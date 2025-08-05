387c4d2435ca947cf9ee3e124cb5b82c0c0d3a8f
SiliconDreamArtists <development@sda.studio>
2025-08-04 23:55:45 -0700

    In progress work

---
+++ Src/Storage/AzureKeyVault/PowerShell/New-ModuleManifest.ps1
@@ -0,0 +1,21 @@
+$root = "$PSScriptRoot"
+if ($root -ne (Get-Location).Path) {
+    Set-Location $root
+}
+
+New-ModuleManifest -Path ./Storage_AzureKeyVault.psd1 `
+  -RootModule 'Storage_AzureKeyVault.psm1' `
+  -ModuleVersion '1.0.0' `
+  -Author 'Silicon Dream Artists' `
+  -CompanyName 'Silicon Dream Artists' `
+  -Description 'Native PowerShell implementation for the Azure Key Vault Storage Adapter.' `
+  -Tags "'Storage_AzureKeyVault' 'SovereignTrust.Adapters' 'SovereignTrust' 'Adapters' 'Public' 'Core' 'SDA' 'BDDB'" `
+  -LicenseUri 'https://opensource.org/licenses/MIT' `
+  -ProjectUri 'https://github.com/SiliconDreamArtists/Storage_AzureKeyVault' `
+  -CompatiblePSEditions 'Core' `
+  -PowerShellVersion '5.1'
+
+  Invoke-GenerateModuleFile -OutputFile 'Storage_AzureKeyVault.psm1' -Root $PSScriptRoot
+  
+
+  
